61a3d2485b86011c8c924ddd480526d48cb63f86
Landon Mossburg <landon.mossburg@gmail.com>
2015-08-12 14:13:54 -0700

    actually print something

--- lazy-professor/single_line_solution.rb
+++ lazy-professor/single_line_solution.rb
@@ -1,1 +1,1 @@
-File.read("#{Dir.pwd}/testa.txt").split("\n").map{|l|l.split(//)}.transpose.map{|a|a.group_by(&:itself).values.max_by(&:size).first}.join
+File.read("#{Dir.pwd}/test.txt").split("\n").map{|l|l.split(//)}.transpose.map{|a|a.group_by(&:itself).values.max_by(&:size).first}.join.each_line{|l|puts(l)}
